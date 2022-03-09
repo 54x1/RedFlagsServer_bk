2be1d5e0fc682fb942ae485fb8b4da51b287e61f
54x1 <five4x1@gmail.com>
2022-03-09 19:40:55 +1100

    update

---
+++ frontend/index.js
@@ -0,0 +1,244 @@
+
+$( ".card-section" ).click(function() {
+    if ( $( this ).hasClass("ui-draggable-dragging") ) {
+
+}
+});
+
+$('.card-section').draggable({
+  cursor: "grabbing",
+  containment: ".place",
+  stack: ".card-section",
+  snapTolerance: 20
+});
+
+
+$('.flags').sortable();
+
+
+$('.card-section').on('touchmove', function(){
+// $(this).css({"left"}) =
+});
+let socket = io();
+let pppperksss;
+let qperks = []
+let qqperk;
+// const socket = io('https://flags-54x1.herokuapp.com/');
+// var perk1val = document.getElementsByClassName('perk3');
+// var perk2val = document.getElementsByClassName('perk4');
+// // console.log(perk2val.value);
+// socket.emit('perks', {
+//        perk1: perk1val.value,
+//        perk2s: perk2val.value
+//    });
+socket.on('init', handleInit);
+socket.on('gameState', handleGameState);
+socket.on('gameOver', handleGameOver);
+socket.on('gameCode', handleGameCode);
+socket.on('perks', handlePerks);
+// socket.on('pperks', joinGame)
+// socket.on('pperks', handlePPerks);
+socket.on('ppperks', handlePPerks);
+socket.on('unknownCode', handleUnknownCode);
+socket.on('tooManyPlayers', handleTooManyPlayers);
+// socket.on('handlePerks', joinPerks);
+
+
+
+function perk(data){
+    // console.log(data);
+    // socket.on('question', function (message) {
+    //   console.log("question", message)
+    //     $('.perk1').append(message);
+    //       });
+
+}
+
+const gameScreen = document.getElementById('gameScreen');
+const initialScreen = document.getElementById('initialScreen');
+const newGameBtn = document.getElementById('newGameButton');
+const joinGameBtn = document.getElementById('joinGameButton');
+const gameCodeInput = document.getElementById('gameCodeInput');
+const gamePerk1 = document.getElementById('perk1');
+const gameCodeDisplay = document.getElementById('gameCodeDisplay');
+const perk1 = document.getElementById('perk1');
+const perk2 = document.getElementById('perk2');
+newGameBtn.addEventListener('click', newGame);
+// newGameBtn.addEventListener('click', joinPerks);
+// joinGameBtn.addEventListener('click', joinGame);
+
+
+// console.log();
+function newGame() {
+  socket.emit('newGame');
+
+// perk();
+  init();
+}
+
+// function joinPerks(){
+//   // console.log(data);
+//   $('.perk1').append(perks[0])
+//         $('.perk2').append(perks[1])
+
+
+// }
+
+
+
+$(joinGameBtn).on('click', function(){
+  $(gameCodeDisplay).html($(gameCodeInput).val());
+  $(perk1).html($(gamePerk1).val());
+  console.log('pppperksssjbtn', pppperksss)
+  socket.emit('perks');
+  const code = gameCodeInput.value;
+  socket.emit('joinGame', code);
+  init();
+})
+
+function joinGame() {
+
+  console.log('pppperksssjfunc', qqperk)
+
+
+  // socket.on('perk1', function (message) {
+  //   console.log("question2", message)
+  //       });
+  
+}
+
+function handlePPerks(pperks){
+  let perks = pperks
+  console.log('pppperkssshh', pperks)
+  perk1.innerText = perks[0];
+  perk2.innerText = perks[1];
+  console.log('hiih', perks)
+}
+
+qperks.push(pppperksss)
+
+
+let playerNumber;
+let gameActive = false;
+
+function init() {
+  initialScreen.style.display = "none";
+  gameScreen.style.display = "block";
+  //
+  // canvas = document.getElementById('canvas');
+  // ctx = canvas.getContext('2d');
+  //
+  // canvas.width = canvas.height = 600;
+  //
+  // ctx.fillStyle = BG_COLOUR;
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //
+  // document.addEventListener('keydown', keydown);
+
+  $.getJSON('flags.json', function(data) {
+    // console.log(data);
+    var randInt = Math.floor(Math.random() * (data.flags.length));
+    var randInt2 = Math.floor(Math.random() * (data.flags.length));
+      var randInt3 = Math.floor(Math.random() * (data.flags.length));
+        var randInt4 = Math.floor(Math.random() * (data.flags.length));
+    $('.flags >.card-section:first-child').html("").append(data.flags[randInt].card);
+    $('.flags >.card-section:nth-child(2)').html("").append(data.flags[randInt2].card);
+    $('.flags >.card-section:nth-child(3)').html("").append(data.flags[randInt3].card);
+    $('.flags >.card-section:last-child').html("").append(data.flags[randInt4].card);
+    // $('.card-section>p:nth-child(3)').append(data.flags[randInt2].card);
+
+  });
+
+  // $.getJSON("perks.json",function(data){
+  //     var randIn = Math.floor(Math.random() * (data.perks.length + 1));
+  //     var randIn2 = Math.floor(Math.random() * (data.perks.length + 1));
+  //     // getPerks(randIn, randIn2, data);
+  //
+  //
+  //     console.log(data);
+  //     // alert('heree1');
+  //
+  // });
+
+
+
+  // $.getJSON('perks.json', function(data) {
+  // console.log(data);
+  //
+  // });
+
+  gameActive = true;
+}
+
+
+
+function paintGame(state) {
+
+}
+
+function handleInit(number) {
+  playerNumber = number;
+}
+
+function handleGameState(gameState) {
+  if (!gameActive) {
+    return;
+  }
+
+
+  gameState = "123"
+}
+
+function handleGameOver(data) {
+  if (!gameActive) {
+    return;
+  }
+  data = JSON.parse(data);
+
+  gameActive = false;
+
+  if (data.winner === playerNumber) {
+    // alert('You Win!');
+  } else {
+    // alert('You Lose :(');
+  }
+}
+
+function handleGameCode(gameCode) {
+  gameCodeDisplay.innerText = gameCode;
+}
+
+function handlePerks(perks){
+  perk1.innerText = perks[0];
+  perk2.innerText = perks[1];
+  pppperksss = perks
+  console.log('heeh', pppperksss)
+
+  socket.emit('pperks', pppperksss);
+}
+
+// function handleGameCode1(gameCode1){
+// $.getJSON("perks.json",function(data){
+//     var randIn = Math.floor(Math.random() * (data.perks.length));
+//     var randIn2 = Math.floor(Math.random() * (data.perks.length));
+//     $('body').append('perkData');
+// });
+// }
+function handleUnknownCode() {
+  reset();
+  alert('Unknown Game Code');
+}
+
+function handleTooManyPlayers() {
+  reset();
+  alert('This game is already in progress');
+}
+
+
+function reset() {
+  playerNumber = null;
+  // $('.perk1, .perk2').html('');
+  gameCodeInput.value = '';
+  initialScreen.style.display = "block";
+  gameScreen.style.display = "none";
+}
---
+++ server/server.js
@@ -0,0 +1,172 @@
+// const io = require('socket.io')();
+const { makeid } = require('./utils');
+
+
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '/../frontend');
+
+// console.log(publicPath)
+
+let jsonData = require(path.join(__dirname, '/../frontend/perks.json'));
+
+console.log(jsonData);
+const port = process.env.PORT || 3000
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+let pp
+
+const state = {};
+const clientRooms = {};
+app.use(express.static(publicPath));
+io.on('connection', client => {
+
+console.log("connected")
+  // client.on('keydown', handleKeydown);
+  client.on('newGame', handleNewGame);
+  client.on('joinGame', handleJoinGame);
+  client.on('pperks', pperksHandle);
+  client.on('perks', perksHandle);
+
+
+
+function handlePerks(){
+  let data = jsonData;
+    var randIn = Math.floor(Math.random() * (data.perks.length));
+    var randIn2 = Math.floor(Math.random() * (data.perks.length));
+    var perkData1 = (data.perks[randIn].card);
+    var perkData2 = (data.perks[randIn2].card);
+    var perks = [perkData1, perkData2];
+    console.log(perks)
+    pp = perks;
+    return perks;
+  // function perks(){
+    
+  // }
+  // let pp = perks();
+  // pperkss = pp;
+  // 
+  // return pp
+
+ }
+ let pperkss;
+function pperksHandle(data){
+  console.log("pp", pp)
+console.log('is here after press new game/', data)
+pperkss = pp
+}
+
+ function perksHandle(){
+console.log('pperkss', pp)
+client.emit('ppperks', pp)
+
+ }
+  function handleJoinGame(roomName) {
+    const room = io.sockets.adapter.rooms[roomName];
+
+    let allUsers;
+    if (room) {
+      allUsers = room.sockets;
+    }
+
+    let numClients = 0;
+    if (allUsers) {
+      numClients = Object.keys(allUsers).length;
+    }
+
+    if (numClients === 0) {
+      client.emit('unknownCode');
+      return;
+    } else if (numClients > 9) {
+      client.emit('tooManyPlayers');
+      return;
+    }
+
+    clientRooms[client.id] = roomName;
+
+    client.join(roomName);
+    client.number = 2;
+    client.emit('init', 2);
+
+    //  handlePerks()
+
+    // io.sockets.in(roomName).emit('handlePerks',  handlePerks());
+  }
+
+
+  function handleNewGame() {
+    
+
+
+    var length = 6;
+    let roomName = makeid(length);
+    clientRooms[client.id] = roomName;
+    client.emit('gameCode', roomName);
+
+    client.join(roomName);
+
+    client.number = 1;
+    client.emit('init', 1);
+    // client.emit('perk', perks())
+    handlePerks()
+    console.log("pp", pp)
+     
+  // io.sockets.in(roomName).emit('perks', pp);
+  client.emit('perks', pp);
+  }
+
+
+
+  // function handleKeydown(keyCode) {
+  //   // const roomName = clientRooms[client.id];
+  //   // if (!roomName) {
+  //   //   return;
+  //   // }
+  //   // try {
+  //   //   keyCode = parseInt(keyCode);
+  //   // } catch(e) {
+  //   //   console.error(e);
+  //   //   return;
+  //   // }
+  //   //
+  //   // const vel = getUpdatedVelocity(keyCode);
+  //   //
+  //   // if (vel) {
+  //   //   state[roomName].players[client.number - 1].vel = vel;
+  //   // }
+  // }
+});
+
+// function startGameInterval(roomName) {
+//   const intervalId = setInterval(() => {
+//     const winner = gameLoop(state[roomName]);
+
+//     if (!winner) {
+//       emitGameState(roomName, state[roomName])
+//     } else {
+//       emitGameOver(roomName, winner);
+//       state[roomName] = null;
+//       clearInterval(intervalId);
+//     }
+//   }, 1000 / FRAME_RATE);
+// }
+
+// function emitGameState(room, gameState) {
+//   // Send this event to everyone in the room.
+//   io.sockets.in(room)
+//     .emit('gameState', JSON.stringify(gameState));
+// }
+
+// function emitGameOver(room, winner) {
+//   io.sockets.in(room)
+//     .emit('gameOver', JSON.stringify({ winner }));
+// }
+
+server.listen(port, ()=>{
+  console.log(`Server is up on port ${port}`);
+})
