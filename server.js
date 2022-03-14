b63a91d967b6258c667825eade33f29294f7ef19
54x1 <five4x1@gmail.com>
2022-03-14 18:39:41 +1100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ let server = http.createServer(app);
 let io = socketIO(server);
 
 let pp
-
+let flagState = []
 let pperkss;
 const clientRooms = {};
 app.use(express.static(publicPath));
@@ -37,7 +37,7 @@ console.log("connected")
 client.on('subFlagCard', subFlagCardHandle)
 client.on('joinFlags', subFlagCardHandle)
 client.on('newJoinFlag', newJoinFlagHandle)
-let flagState = []
+
 
 function handleJoinGame(roomName) {
   const room = io.sockets.adapter.rooms[roomName];
@@ -169,7 +169,7 @@ client.emit('ppperks', pp)
  }
  client.on('disconnect', ()=>{
   console.log('disconnect', client.id)
-  flagState = ""
+  // flagState = ""
   })
 
 });
