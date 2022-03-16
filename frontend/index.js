65e1c2d1310a13231bc6077d208fef6fedfabfb7
54x1 <five4x1@gmail.com>
2022-03-16 22:36:07 +1100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -97,8 +97,7 @@ cards.push($(this).text())
 })
 console.log(cards)
 let code = gameCodeDisplay.innerText
-// socket.emit('subFlagCard', {room:[{code},{cards}]})
-socket.to(code).emit('subFlagCard', {room:[{code},{cards}]});
+socket.emit('subFlagCard', {room:[{code},{cards}]})
 // socket.emit('flag', $(this).html());
 $("#sign").bind('click', function(){ return false; });
 $("#sign").css( {"cursor":"not-allowed"});
