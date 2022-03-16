ee1a337662a651ca0c2ed353ac09389c8633a8a5
54x1 <five4x1@gmail.com>
2022-03-17 03:45:34 +1100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -60,7 +60,7 @@ console.log("subFlagData", data)
 // $('.public-flags').append("<div class='card-section text-center'>"+data.room[1].cards"</div>")
 $(data.room[1].cards).each(function (){
   console.log('insde each')
-  $('.public-flags').append("<div class='card-section text-center'>"+data.room[1].cards+"</div>")
+  $('.public-flags').append("<div class='card-section text-center'>"+data.room[1].cards.cards+"</div>")
 })
 }
 }
